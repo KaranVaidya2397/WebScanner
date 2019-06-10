a7be0f72d7843eb35c274af2eb2be4d1af439634
KaranVaidya2397 <43272337+KaranVaidya2397@users.noreply.github.com>
2019-06-10 10:42:19 +0530

    Update general.py

--- general.py
+++ general.py
@@ -1,1 +1,8 @@
 import os
+def create_dir(directory):
+  if not os.path_exists(directory):
+    os.makedir(directory)
+def write_file(path, data):
+  f = open(path, 'w')
+  f.write(data)
+  f.close()
