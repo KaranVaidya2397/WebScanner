3d45d3c9ea7c66c437a5966b7891395ded22736c
KaranVaidya2397 <43272337+KaranVaidya2397@users.noreply.github.com>
2019-06-11 09:54:21 +0530

    Update robots_txt.py

--- robots_txt.py
+++ robots_txt.py
@@ -1,1 +1,10 @@
-
+import urllib.request
+import io
+def get_robots_txt(url):
+  if url.endsWith('/'):
+    path = url
+  else
+    path = url + '/'
+  req = urllib.request.urlopen(path + "robots.txt", data=NONE)
+  data = io.IOTextWrapper(req, encoding="utf-8")
+  return data
